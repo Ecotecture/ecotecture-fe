166e9c4d350ab02e4665b92e81ab4d260cc3edcf
Maria Aurellia <mariaadannies26@gmail.com>
2024-01-12 00:34:19 +0700

    modify navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,25 +8,25 @@ export default function Navbar() {
 
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <div className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
-            <button type="button" className="text-white bg-[#257251] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-4 py-2 text-center">Login</button>
+            <button type="button" className="text-white bg-[#257251] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-4 py-2 text-center">Login</button>
             <br />
-            <button type="button" className="text-black bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-4 py-2 text-center border-solid border-2 border-[#257251]">Create Account</button>
+            <button type="button" className="text-black bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-4 py-2 text-center border-solid border-2 border-[#257251]">Create Account</button>
           </div>
 
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A]" aria-current="page">Home</a>
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A] no-underline hover:underline hover:underline-offset-8 hover:decoration-[#257251] hover:decoration-2" aria-current="page">Home</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A]">About</a>
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A] no-underline hover:underline hover:underline-offset-8 hover:decoration-[#257251] hover:decoration-2">About</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A]">Contact</a>
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A] no-underline hover:underline hover:underline-offset-8 hover:decoration-[#257251] hover:decoration-2">Contact</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A]">Help</a>
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A] no-underline hover:underline hover:underline-offset-8 hover:decoration-[#257251] hover:decoration-2">Help</a>
             </li>
           </ul>
         </div>
