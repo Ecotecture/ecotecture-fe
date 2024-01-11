88534e1a098d18eded083a3a92995f6f4a200e1c
Maria Aurellia <mariaadannies26@gmail.com>
2024-01-12 00:16:14 +0700

    setup next and layout (navbar, footer)

---
+++ components/Footer.tsx
@@ -0,0 +1,83 @@
+export default function Footer() {
+    return (
+        <footer className="bg-white">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
+                    <div>
+                        <h2 className="mb-6 font-serif text-sm font-semibold text-gray-900 uppercase">Product</h2>
+                        <ul className="text-gray-500 font-medium">
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Individuals</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Team</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Technology</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Integrations</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className="mb-6 font-serif text-sm font-semibold text-gray-900 uppercase">Product</h2>
+                        <ul className="text-gray-500 font-medium">
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Individuals</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Team</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Technology</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Integrations</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className="mb-6 font-serif text-sm font-semibold text-gray-900 uppercase">Product</h2>
+                        <ul className="text-gray-500 font-medium">
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Individuals</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Team</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Technology</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Integrations</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className="mb-6 font-serif text-sm font-semibold text-gray-900 uppercase">Product</h2>
+                        <ul className="text-gray-500 font-medium">
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Individuals</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Team</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Technology</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="#" className="font-sans hover:underline">Integrations</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <span className="text-sm text-black sm:text-center">© 2024 Ecotecture. All Rights Reserved.
+                    </span>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,39 @@
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white fixed w-full top-0 z-20 start-0 border-b border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        </a>
+
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+            <button type="button" className="text-white bg-[#257251] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-4 py-2 text-center">Login</button>
+            <br />
+            <button type="button" className="text-black bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-4 py-2 text-center border-solid border-2 border-[#257251]">Create Account</button>
+          </div>
+
+        </div>
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+            <li>
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A]" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A]">About</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A]">Contact</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-[#272A5A]">Help</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </nav>
+
+
+  )
+}
